ac9b9cd68ef02a4d677becc6e2fda79dd411e319
cyrus chen <pschen@lcs.on.ca>
2023-02-07 08:53:50 -0500

    Initial Commit

---
+++ SpendingAmountCalculator/SpendingAmountCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendingAmountCalculatorApp.swift
+//  SpendingAmountCalculator
+//
+//  Created by Cyrus Chen on 7/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpendingAmountCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
